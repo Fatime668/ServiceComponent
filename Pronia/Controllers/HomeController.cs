ffa289a61df3f9448a03e08e7f1ef772f8dc2128
Fatime668 <tu7dkjdue@code.edu.az>
2022-06-01 12:38:45 +0400

    Proniaaa

---
+++ Pronia/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Models;
+using Pronia.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeVM model = new HomeVM
+            {
+                Sliders = await _context.Sliders.ToListAsync(),
+                Clients = await _context.Clients.ToListAsync(),
+                Plants = await _context.Plants.Include(p => p.PlantImages).ToListAsync(),
+                AnotherSetting = await _context.AnotherSettings.FirstOrDefaultAsync()
+              };
+            
+            return View(model);
+        }
+    }
+}
---
+++ Pronia/Services/LayoutService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pronia.Services
+{
+    public class LayoutService
+    {
+        private readonly AppDbContext _context;
+
+        public LayoutService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<AnotherSetting> GetDatas()
+        {
+
+            AnotherSetting anothersetting = await _context.AnotherSettings.FirstOrDefaultAsync();
+            return anothersetting;
+        }
+    }
+}
